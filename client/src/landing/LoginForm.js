e7906195f914214f336537e77fcdb1bc7179d2af
Пользователь <Salmak_Asyl>
2022-03-09 19:33:29 +0600

    Login with server

--- client/src/landing/LoginForm.js
+++ client/src/landing/LoginForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { Button, Checkbox, Form } from "semantic-ui-react";
 import { useState } from "react";
 import "./index.css";
@@ -6,25 +7,58 @@ import "./index.css";
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [confirm, setConfirm] = useState(false)
   return (
     <Form className="login-form">
       <Form.Field>
         <label>Email</label>
-        <input value={email} onChange={(e) => {
-            setEmail(e.target.value)
-        }} placeholder="email" />
+        <input
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="email"
+        />
       </Form.Field>
       <Form.Field>
         <label>Password</label>
-        <input value={password} onChange={(e) => {
-             setPassword(e.target.value)
-        }} placeholder="Password" />
+        <input
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder="Password"
+        />
       </Form.Field>
       <Form.Field>
-        <Checkbox label="I agree to the Terms and Conditions" />
+        <Checkbox checked={
+         confirm
+        } onChange={()=>{
+          setConfirm(!confirm)
+        }} label="I confirm my data" />
       </Form.Field>
-      <Button type="submit">Submit</Button>
+      <Button
+        color="green"
+        disabled={!(email && password && confirm)} 
+        onClick={() => {
+          axios({
+            method: "post",
+            url: "messages",
+            baseURL: "http://127.0.0.1:3000",
+            data: { login: email, password: password },
+          })
+            .then((response) => {
+              console.log(response)
+              alert(response.data);
+            })
+            .catch((error) => {
+              alert(error);
+            });
+        }}
+        type="submit"
+      >
+        Submit
+      </Button>
     </Form>
   );
 };
