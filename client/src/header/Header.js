08d4f2ac076b7039726800e42149bfbd37934881
Abdygapparov-devpro <yerden.abdygapparov@dev.pro>
2022-05-03 18:55:36 +0600

    .

--- client/src/header/Header.js
+++ client/src/header/Header.js
@@ -1,40 +1,43 @@
-import React from "react";
-import { Menu, Container } from "semantic-ui-react";
-import LoginDropdownMenu from "../landing/LoginDropdownMenu";
-import logo from "../public/pngwing3.png";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import "./index.css";
+import React from 'react';
+import { Menu, Container } from 'semantic-ui-react';
+import LoginDropdownMenu from '../landing/LoginDropdownMenu';
+import logo from '../public/pngwing3.png';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import './index.css';
 
 const Header = () => {
-  const user = useSelector((store) => store.user);
+  const user = useSelector(store => store.user);
   return (
-    <div className="Header">
+    <div className='Header'>
       <Container>
         <Menu secondary>
-          <img
-            src={logo}
-            style={{ width: "150px", height: "70px", margin: "5px" }}
-          />
+          <Link to='/'>
+            <img
+              src={logo}
+              style={{ width: '150px', height: '70px', margin: '5px' }}
+            />
+          </Link>
+
           {user.logedIn ? (
             <Menu.Item>
-              <Link to="./private/dashboard" className="link">
+              <Link to='./private/dashboard' className='link'>
                 Мой Банк
               </Link>
             </Menu.Item>
           ) : null}
           <Menu.Item>
-            <a href="#section3">Новости</a>
+            <a href='#section3'>Новости</a>
           </Menu.Item>
           <Menu.Item>
-            <Link className="link" to="/">
+            <Link className='link' to='/'>
               Услуги
             </Link>
           </Menu.Item>
           <Menu.Item>
-            <a href="">Контакты</a>
+            <a href=''>Контакты</a>
           </Menu.Item>
-          <Menu.Menu position="right">
+          <Menu.Menu position='right'>
             <Menu.Item>
               <LoginDropdownMenu />
             </Menu.Item>
